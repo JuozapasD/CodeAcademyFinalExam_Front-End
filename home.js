94921832be552deb8375f0a192343d26e658a786
Deividas Juozapavicius <juozapaviciusd@gmail.com>
2023-02-21 20:57:50 +0200

    Updated user window when user first logins. It shows list of all users. Updated login and register screens

--- home.js
+++ home.js
@@ -1,4 +1,10 @@
-const tableBody = document.querySelector("#cars-table tbody");
+const table = document.getElementById("accounts-table");
+const tableBody = document.getElementById("accounts-table-body");
+const addUserButton = document.querySelector("#add-user-info");
+
+addUserButton.addEventListener("click", () => {
+  window.location.href = "/add-user-info.html";
+});
 
 const getCookie = name => {
   const cookies = document.cookie.split(";");
@@ -39,36 +45,43 @@ getRole();
 
 // prideti if, kad getrole == admin // getrole == user
 
-if (getRole() == "Admin" || getRole() == "User") {
-  async function fetchCars() {
-    try {
-      const response = await fetch("http://localhost:5136/Api/Car", {
+async function fetchAccounts() {
+  try {
+    const response = await fetch(
+      "https://localhost:7112/api/get-all-accounts",
+      {
         headers: {
           Authorization: `Bearer ${getCookie("token")}`
         }
-      });
-      if (response.ok) {
-        const cars = await response.json();
-        cars.forEach(car => {
-          const row = document.createElement("tr");
-          const brandCell = document.createElement("td");
-          brandCell.textContent = car.brand;
-          const idCell = document.createElement("td");
-          idCell.textContent = car.id;
-          const colorCell = document.createElement("td");
-          colorCell.textContent = car.color;
+      }
+    );
 
-          // delete button gimimo vieta
-          // CIA IKISTI IF, KAD USERIAI NEMATYTU DELETE BUTTON
-          /////// PVZ IDETI PARAMETRA :DISABLED
+    if (response.ok) {
+      const accounts = await response.json();
 
+      accounts.forEach(account => {
+        const row = document.createElement("tr");
+        row.classList.add("account-row");
+
+        const usernameCell = document.createElement("td");
+        usernameCell.textContent = account.username;
+
+        const roleCell = document.createElement("td");
+        roleCell.textContent = account.role;
+
+        const deleteCell = document.createElement("td");
+        deleteCell.id = "delete-column";
+
+        // Create the delete button only for the "Admin" role
+        if (getRole() === "Admin") {
           const deleteButton = document.createElement("button");
+          deleteButton.classList.add("delete-button");
           deleteButton.textContent = "Delete";
-          deleteButton.style.marginLeft = "10px";
+
           deleteButton.addEventListener("click", async () => {
             try {
               const response = await fetch(
-                `http://localhost:5136/Api/Car/${car.id}`,
+                `https://localhost:7112/api/accounts/${account.id}`,
                 {
                   method: "DELETE",
                   headers: {
@@ -76,42 +89,32 @@ if (getRole() == "Admin" || getRole() == "User") {
                   }
                 }
               );
+
               if (response.ok) {
                 row.remove();
               } else {
-                console.error("Failed to delete car");
+                console.error("Failed to delete account");
               }
             } catch (error) {
               console.error(error);
             }
           });
 
-          //////////////////////////////////////
+          deleteCell.appendChild(deleteButton);
+        }
 
-          brandCell.style.fontWeight = "bold";
-          brandCell.style.textTransform = "uppercase";
-          brandCell.style.border = "1px solid gray";
-          brandCell.style.padding = "10px";
-          idCell.style.padding = "10px";
-          idCell.style.border = "1px solid gray";
-          idCell.style.padding = "10px";
-          colorCell.style.padding = "10px";
-          colorCell.style.border = "1px solid gray";
-          colorCell.style.padding = "10px";
+        row.appendChild(usernameCell);
+        row.appendChild(roleCell);
+        row.appendChild(deleteCell);
 
-          row.appendChild(idCell);
-          row.appendChild(brandCell);
-          row.appendChild(colorCell);
-          row.appendChild(deleteButton);
-          tableBody.appendChild(row);
-        });
-      } else {
-        console.error("Failed to fetch cars");
-      }
-    } catch (error) {
-      console.error(error);
+        tableBody.appendChild(row);
+      });
+    } else {
+      console.error("Failed to fetch accounts");
     }
+  } catch (error) {
+    console.error(error);
   }
-
-  fetchCars();
 }
+
+fetchAccounts();
